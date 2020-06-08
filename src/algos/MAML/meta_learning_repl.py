9fbc1b88ef0df23f097b80b138b3fcdc454f78d2
Teymur Azayev <azayetey@fel.cvut.cz>
2020-06-08 14:48:17 +0200

    m

--- src/algos/MAML/meta_learning_repl.py
+++ src/algos/MAML/meta_learning_repl.py
@@ -9,28 +9,43 @@ class SinTask:
     def __init__(self):
         self.a = np.random.rand() * 4 + 1
         self.b = np.random.rand() * np.pi
+        self.dataset_generated = False
 
-    def get_trn_data(self, n):
-        self.X = np.linspace(0, 2 * np.pi, 2 * n).astype(np.float32)
-        self.Y = self.a * np.sin(self.b + self.X)
-        indeces = np.arange(n * 2)
-        np.random.shuffle(indeces)
-        self.trn_indeces = indeces[:n]
-        self.tst_indeces = indeces[n:]
-        return self.X[self.trn_indeces], self.Y[self.trn_indeces]
 
-    def get_tst_data(self):
-        return self.X[self.tst_indeces], self.Y[self.tst_indeces]
+    def get_dataset(self, n):
+        if not self.dataset_generated:
+            self.X = np.linspace(0, 2 * np.pi, 2 * n).astype(np.float32)
+            self.Y = self.a * np.sin(self.b + self.X)
+            indeces = np.arange(n * 2)
+            np.random.shuffle(indeces)
+            self.trn_indeces = indeces[:n]
+            self.tst_indeces = indeces[n:]
+            self.X_trn = self.X[self.trn_indeces]
+            self.Y_trn = self.Y[self.trn_indeces]
+            self.X_tst = self.X[self.tst_indeces]
+            self.Y_tst = self.Y[self.tst_indeces]
+            self.dataset_generated = True
 
-    def get_trn_tst_data_HC(self, n):
+        return self.X_trn, self.Y_trn, self.X_tst, self.Y_tst
+
+
+    def get_dataset_halfsin(self, n):
         Xtrn = np.linspace(0, 2 * np.pi, n).astype(np.float32)
         Xtst = np.linspace(np.pi, 2 * np.pi, n).astype(np.float32)
         Ytrn = self.a * np.sin(self.b + Xtrn)
         Ytst = self.a * np.sin(self.b + Xtst)
-        return Xtrn, Xtst, Ytrn, Ytst
+        return Xtrn, Ytrn, Xtst, Ytst
 
-    def plot(self, *args, **kwargs):
-        return plt.plot(self.X, self.Y, *args, **kwargs)
+
+    def plot_trn_set(self):
+        if not self.dataset_generated:
+            self.get_dataset(30)
+        return plt.plot(self.X_trn, self.Y_trn, 'ro', self.X_tst, self.Y_tst, 'go')
+
+
+    def plot_trn_set_halfsin(self):
+        X_trn, Y_trn, X_tst, Y_tst = self.get_dataset_halfsin(15)
+        return plt.plot(X_trn, Y_trn, 'ro', X_tst, Y_tst, 'go')
 
 
 class SinPolicy(nn.Module):
@@ -59,17 +74,19 @@ def train_fomaml(env_fun, param_dict):
 
         # Sample tasks
         env_list = [env_fun() for _ in range(param_dict["batch_tasks"])]
+        dataset_list = [env.get_dataset(param_dict["batch_trn"]) for env in env_list]
 
         # Updated params list
         copied_meta_policy_list = []
 
         trn_losses = []
-        for env in env_list:
+        for ei, env in enumerate(env_list):
             # Get data
-            Xtrn, Ytrn = env.get_trn_data(param_dict["batch_trn"])
+            Xtrn, Ytrn, _, _ = dataset_list[ei]
 
             # Copy parameters to new network
             copied_meta_policy = deepcopy(meta_policy)
+            copied_meta_policy_list.append(copied_meta_policy)
 
             # Evaluate gradient and updated parameter th_i on sampled task
             trn_opt = T.optim.SGD(copied_meta_policy.parameters(), lr=param_dict["lr"], momentum=param_dict["momentum_trn"])
@@ -78,15 +95,16 @@ def train_fomaml(env_fun, param_dict):
                 Yhat = copied_meta_policy(T.from_numpy(Xtrn).unsqueeze(1))
                 loss = lossfun(Yhat, T.from_numpy(Ytrn).unsqueeze(1))
                 trn_losses.append(loss.detach().numpy())
+                trn_opt.zero_grad()
                 loss.backward()
                 trn_opt.step()
 
-            copied_meta_policy_list.append(copied_meta_policy)
+            trn_opt.zero_grad()
 
         tst_losses = []
         # Calculate loss on test task
-        for env, policy_i in zip(env_list, copied_meta_policy_list):
-            Xtst, Ytst = env.get_tst_data()
+        for env, policy_i, dataset in zip(env_list, copied_meta_policy_list, dataset_list):
+            _, _, Xtst, Ytst = dataset
             Yhat = policy_i(T.from_numpy(Xtst).unsqueeze(1))
             loss = lossfun(Yhat, T.from_numpy(Ytst).unsqueeze(1))
             tst_losses.append(loss.detach().numpy())
@@ -112,53 +130,62 @@ def train_fomaml(env_fun, param_dict):
                                                                                      np.mean(trn_losses),
                                                                                      np.mean(tst_losses)))
 
+    plt.ion()
+
     # Test the meta learned policy to adapt to a new task after n gradient steps
-    env = env_fun()
+    test_env_1 = env_fun()
+    Xtrn_1, Ytrn_1, X_tst_1, Y_tst_1 = test_env_1.get_dataset(param_dict["batch_trn"])
+    # Do training and evaluation on normal dataset
+    policy_test_1 = deepcopy(meta_policy)
+    opt_test = T.optim.SGD(policy_test_1.parameters(), lr=param_dict["lr"], momentum=param_dict["momentum_trn"])
+    for t in range(param_dict["training_iters"]):
+        Yhat = policy_test_1(T.from_numpy(Xtrn_1).unsqueeze(1))
+        loss = lossfun(Yhat, T.from_numpy(Ytrn_1).unsqueeze(1))
+        opt_test.zero_grad()
+        loss.backward()
+        opt_test.step()
 
-    if False:
-        Xtrn, Ytrn = env.get_trn_data(param_dict["batch_trn"])
-        # Do training and evaluation on normal dataset
-        policy_normal = deepcopy(meta_policy)
-        opt = T.optim.SGD(policy_normal.parameters(), lr=param_dict["lr"], momentum=param_dict["momentum_trn"])
-        for t in range(param_dict["training_iters"]):
-            Yhat = policy_normal(T.from_numpy(Xtrn).unsqueeze(1))
-            loss = lossfun(Yhat, T.from_numpy(Ytrn).unsqueeze(1))
-            loss.backward()
-            opt.step()
+    Yhat_tst_1 = policy_test_1(T.from_numpy(X_tst_1).unsqueeze(1)).detach().numpy()
 
-        Yhat_tst = policy_normal(T.from_numpy(env.X).unsqueeze(1)).detach().numpy()
+    test_env_2 = env_fun()
+    Xtrn_2, Ytrn_2, Xtst_2, Ytst_2 = test_env_2.get_dataset_halfsin(param_dict["batch_trn"])
+    # Do training and evaluation on hardcore dataset
+    policy_test_2 = deepcopy(meta_policy)
+    opt = T.optim.SGD(policy_test_2.parameters(), lr=param_dict["lr"], momentum=param_dict["momentum_trn"])
+    for t in range(param_dict["training_iters"]):
+        Yhat = policy_test_2(T.from_numpy(Xtrn_2).unsqueeze(1))
+        loss = lossfun(Yhat, T.from_numpy(Ytrn_2).unsqueeze(1))
+        loss.backward()
+        opt.step()
 
-    if True:
-        Xtrn, Ytrn, Xtst, Ytst = env.get_trn_tst_data_HC(param_dict["batch_trn"])
-        # Do training and evaluation on hardcore dataset
-        policy_normal = deepcopy(meta_policy)
-        opt = T.optim.SGD(policy.parameters(), lr=param_dict["lr"], momentum=param_dict["momentum_trn"])
-        for t in range(param_dict["training_iters"]):
-            Yhat = policy_normal(T.from_numpy(Xtrn).unsqueeze(1))
-            loss = lossfun(Yhat, T.from_numpy(Ytrn).unsqueeze(1))
-            loss.backward()
-            opt.step()
+    Yhat_tst_2 = policy_test_2(T.from_numpy(Xtst_2).unsqueeze(1)).detach().numpy()
 
-        env.get_trn_data(param_dict["batch_trn"])
-        Yhat_tst = policy_normal(T.from_numpy(env.X).unsqueeze(1)).detach().numpy()
-
-    env.plot()
-    plt.plot(env.X, Yhat_tst, "r")
+    plt.figure()
+    test_env_1.plot_trn_set()
+    plt.plot(Xtrn_1, Yhat_tst_1, "bo")
+    plt.title("Env_1")
     plt.show()
+    plt.pause(.001)
+
+    plt.figure()
+    plt.title("Env_2")
+    test_env_2.plot_trn_set()
+    plt.plot(Xtrn_2, Yhat_tst_2, "bo")
+    plt.show()
+    plt.pause(1000)
 
 if __name__ == "__main__":
     policy = SinPolicy(24)
 
-    param_dict = {"meta_training_iters" : 1000,
-                  "training_iters": 3,
+    param_dict = {"meta_training_iters" : 10,
+                  "training_iters": 1,
                   "hidden" : 24,
                   "batch_tasks" : 24,
                   "batch_trn" : 16,
                   "lr" : 0.01,
-                  "lr_meta" : 0.003,
+                  "lr_meta" : 0.001,
                   "momentum_trn" : 0.95,
                   "momentum_meta" : 0.95}
 
     env_fun = SinTask
     train_fomaml(env_fun, param_dict)
-
