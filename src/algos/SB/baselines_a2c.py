a75969814acc2c161316f8447adab2016bf4b4c9
silverjoda <heizen.sh@gmail.com>
2020-07-16 22:23:51 +0200

    f

--- src/algos/SB/baselines_a2c.py
+++ src/algos/SB/baselines_a2c.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
               "ID": ID}
 
     print(params)
-    TRAIN = False
+    TRAIN = True
 
     if TRAIN or socket.gethostname() == "goedel":
         n_envs = 6
