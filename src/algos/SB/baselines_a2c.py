b2eec1fe99c341a6873bd09d762ee6de9a122378
silverjoda <heizen.sh@gmail.com>
2020-07-29 09:39:35 +0200

    Training without contacts

--- src/algos/SB/baselines_a2c.py
+++ src/algos/SB/baselines_a2c.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
                   variable_velocity=False)
 
     if not TRAIN:
-        model = A2C.load("agents/8CZ_SB_policy.zip") # 4TD & 8CZ contactless:perlin:normal, U79 & BMT contactless:perlin:extreme, KIH turn_left, 266 turn_rigt
+        model = A2C.load("agents/TY8_SB_policy.zip") # 4TD & 8CZ contactless:perlin:normal, U79 & BMT contactless:perlin:extreme, KIH turn_left, 266 turn_rigt
         #model = A2C.load("agents_best/best_model.zip")  # 2Q5
     #print(evaluate_policy(model, env, n_eval_episodes=3))
 
--- src/envs/bullet_nexabot/hexapod/hexapod_wip.py
+++ src/envs/bullet_nexabot/hexapod/hexapod_wip.py
@@ -85,7 +85,7 @@ class HexapodBulletEnv(gym.Env):
         self.mesh_scale_lat = 0.1
         self.mesh_scale_vert = 2
         self.lateral_friction = 1.2
-        self.training_difficulty = 0.1
+        self.training_difficulty = 0.99
         self.training_difficulty_increment = 0.00015
 
         # Environment parameters
@@ -464,7 +464,7 @@ class HexapodBulletEnv(gym.Env):
         quantile_pen = symmetry_work_pen = contact_rew = 0
 
         if self.training_mode == "straight":
-            r_neg = {"pitch" : np.square(pitch) * 0.5 * self.training_difficulty,
+            r_neg = {"pitch" : np.square(pitch) * 1.5 * self.training_difficulty,
                     "roll" : np.square(roll) * 0.5 * self.training_difficulty,
                     "zd" : np.square(zd) * 0.1 * self.training_difficulty,
                     "yd" : np.square(yd) * 0.2 * self.training_difficulty,
@@ -472,12 +472,13 @@ class HexapodBulletEnv(gym.Env):
                     "thd": np.square(thd) * 0.01 * self.training_difficulty,
                     "quantile_pen" : quantile_pen * 0.0 * self.training_difficulty * (self.step_ctr > 10),
                     "symmetry_work_pen" : symmetry_work_pen * 0.0 * self.training_difficulty * (self.step_ctr > 10),
-                    "torso_contact_pen": torso_contact_pen * 0.1 * self.training_difficulty,
-                    "total_work_pen" : np.minimum(total_work_pen * 0.01 * self.training_difficulty * (self.step_ctr > 10), 1),
-                    "unsuitable_position_pen" : unsuitable_position_pen * 0.01 * self.training_difficulty}
+                    "torso_contact_pen": torso_contact_pen * 0.2 * self.training_difficulty,
+                    "total_work_pen" : np.minimum(total_work_pen * 0.00 * self.training_difficulty * (self.step_ctr > 10), 1),
+                    "unsuitable_position_pen" : unsuitable_position_pen * 0.00 * self.training_difficulty}
             r_pos = {"velocity_rew" : np.clip(velocity_rew * 4, -1, 1),
                      "yaw_improvement_reward" :  np.clip(yaw_improvement_reward * 3., -1, 1),
                      "contact_rew" : contact_rew * 0}
+            print(r_neg)
             r_pos_sum = sum(r_pos.values())
             r_neg_sum = sum(r_neg.values()) * (self.step_ctr > 5)
             r = np.clip(r_pos_sum - r_neg_sum, -3, 3)
