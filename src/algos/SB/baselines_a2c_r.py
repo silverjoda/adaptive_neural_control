41633495ee64c580f08689cb82bed21f7ba91f38
Teymur Azayev <azayetey@fel.cvut.cz>
2020-07-30 12:15:00 +0200

    Add tb log dirs

--- src/algos/SB/baselines_a2c_r.py
+++ src/algos/SB/baselines_a2c_r.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     from src.envs.bullet_nexabot.hexapod.hexapod_wip import HexapodBulletEnv as env_fun
     ID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
-    params = {"iters": 30000000,
+    params = {"iters": 80000000,
               "batchsize": 60,
               "max_steps": 100,
               "gamma": 0.99,
