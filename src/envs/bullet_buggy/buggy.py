66728f080055e8971ea538ab2fdf32d1884c1b6e
Teymur Azayev <azayetey@fel.cvut.cz>
2020-09-08 14:44:18 +0200

    quad env with randomization

--- src/envs/bullet_buggy/buggy.py
+++ src/envs/bullet_buggy/buggy.py
@@ -6,37 +6,47 @@ import numpy as np
 import pybullet as p
 import pybullet_data
 from gym import spaces
+import torch as T
 
 import src.my_utils as my_utils
 
 # Variable params: Mass, boom length, motor "inertia", motor max_force
 class BuggyBulletEnv(gym.Env):
-    def __init__(self, animate=False, max_steps=300, action_input=False, latent_input=False, is_variable=False):
-        if (animate):
-          self.client_ID = p.connect(p.GUI)
+    def __init__(self, config):
+        self.seed = config["seed"]
+        if self.seed is not None:
+            np.random.seed(self.seed)
+            T.manual_seed(self.seed)
         else:
-          self.client_ID = p.connect(p.DIRECT)
+            rnd_seed = int((time.time() % 1) * 10000000)
+            np.random.seed(rnd_seed)
+            T.manual_seed(rnd_seed + 1)
 
-        self.animate = animate
-        self.latent_input = latent_input
-        self.action_input = action_input
-        self.is_variable = is_variable
+        self.animate = config["animate"]
+        self.max_steps = config["max_steps"]
+        self.latent_input = config["latent_input"]
+        self.action_input = config["action_input"]
+        self.is_variable = config["is_variable"]
+        self.sim_timestep = config["sim_timestep"]
+        self.urdf_name = config["urdf_name"]
 
-        # Simulator parameters
-        self.max_steps = max_steps
-        self.animate = animate
-        self.latent_dim = 3
-        self.obs_dim = 10 + self.latent_dim * int(self.latent_input) + int(self.action_input) # Orientation quaternion, linear + angular velocities
-        self.act_dim = 2 # Motor commands (4)
+        self.obs_dim = 10 + int(self.action_input) * 2  # Orientation quaternion, linear + angular velocities
+        self.act_dim = 2  # Motor commands
 
-        self.timeStep = 0.02
+        # Episode variables
         self.step_ctr = 0
 
+        if (self.animate):
+            self.client_ID = p.connect(p.GUI)
+        else:
+            self.client_ID = p.connect(p.DIRECT)
+
         p.setGravity(0, 0, -9.8)
-        p.setTimeStep(self.timeStep)
         p.setRealTimeSimulation(0)
+        p.setTimeStep(self.sim_timestep)
         p.setAdditionalSearchPath(pybullet_data.getDataPath(), physicsClientId=self.client_ID)
 
+        self.robot = None
         self.load_robot()
         self.plane = p.loadURDF("plane.urdf", physicsClientId=self.client_ID)
 
@@ -55,14 +65,43 @@ class BuggyBulletEnv(gym.Env):
         self.steeringSlider = p.addUserDebugParameter("steering", -0.5, 0.5, 0)
 
     def load_robot(self):
+        # Remove old robot
+        if self.robot is not None:
+            p.removeBody(self.robot)
+
         # Randomize robot params
-        self.robot_params = {"mass": 1, "boom": 0.2, "motor_inertia": 1.0, "motor_max_force": 1.0}
+        self.robot_params = {"mass": 1 + np.random.rand() * 0.5,
+                             "wheel_base" : 1 + np.random.rand() * 0.5,
+                             "wheel_width": 1 + np.random.rand() * 0.5,
+                             "front_wheels_friction": 0.5 + np.random.rand() * 2.5,
+                             "motor_force_multiplier": 50 + np.random.rand() * 30}
 
         # Write params to URDF file
-        # ...
+        with open(self.urdf_name, "r") as in_file:
+            buf = in_file.readlines()
+
+        index = self.urdf_name.find('.urdf')
+        output_urdf = self.urdf_name[:index] + '_rnd' + self.urdf_name[index:]
+
+        # Change link lengths in urdf
+        # with open(output_urdf, "w") as out_file:
+        #     for line in buf:
+        #         if "<cylinder radius" in line:
+        #             out_file.write(f'          <cylinder radius="0.015" length="{self.robot_params["boom"]}"/>\n')
+        #         elif line.rstrip('\n').endswith('<!--boomorigin-->'):
+        #             out_file.write(
+        #                 f'        <origin xyz="0 {self.robot_params["boom"] / 2.} 0.0" rpy="-1.5708 0 0" /><!--boomorigin-->\n')
+        #         elif line.rstrip('\n').endswith('<!--motorpos-->'):
+        #             out_file.write(
+        #                 f'      <origin xyz="0 {self.robot_params["boom"]} 0" rpy="0 0 0"/><!--motorpos-->\n')
+        #         else:
+        #             out_file.write(line)
 
         # Load urdf
-        self.robot = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "racecar/racecar.urdf"), physicsClientId=self.client_ID)
+        self.robot = p.loadURDF("buggy.urdf", physicsClientId=self.client_ID)
+
+        # Change params
+        #p.changeDynamics(self.robot, -1, mass=self.robot_params["mass"])
 
     def get_obs(self):
         torso_pos, torso_quat = p.getBasePositionAndOrientation(self.robot, physicsClientId=self.client_ID)
@@ -160,5 +199,9 @@ class BuggyBulletEnv(gym.Env):
 
 
 if __name__ == "__main__":
-    env = QuadrotorBulletEnv(animate=True)
+    import yaml
+    with open("../../algos/SB/configs/buggy_config.yaml") as f:
+        env_config = yaml.load(f, Loader=yaml.FullLoader)
+    env_config["animate"] = True
+    env = BuggyBulletEnv(env_config)
     env.demo()
