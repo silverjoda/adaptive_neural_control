b5663ea28ac2573ed4b466eb3188ae65ce8a010d
Teymur Azayev <azayetey@fel.cvut.cz>
2020-09-11 15:54:37 +0200

    quad env with randomization

--- src/algos/MAML/policies.py
+++ src/algos/MAML/policies.py
@@ -4,64 +4,52 @@ import torch as T
 import numpy as np
 from copy import deepcopy
 
-
 class NN_PG(nn.Module):
     def __init__(self, env, config):
         super(NN_PG, self).__init__()
         self.obs_dim = env.obs_dim
         self.act_dim = env.act_dim
 
-        if obs_dim is not None:
-            self.obs_dim = obs_dim
-
-        if act_dim is not None:
-            self.act_dim = act_dim
-
-        self.tanh = tanh
+        self.tanh = config["policy_lastlayer_tanh"]
+        self.hid_dim = config["policy_hid_dim"]
+        self.grad_clip_value = config["policy_grad_clip_value"]
+        self.policy_residual_connection = config["policy_residual_connection"]
         self.activation_fun = F.leaky_relu
 
-        self.fc1 = nn.Linear(self.obs_dim, hid_dim)
-        self.m1 = nn.LayerNorm(hid_dim)
-        self.fc2 = nn.Linear(hid_dim, hid_dim)
-        self.m2 = nn.LayerNorm(hid_dim)
-        self.fc3 = nn.Linear(hid_dim, self.act_dim)
+        self.fc1 = nn.Linear(self.obs_dim, self.hid_dim)
+        self.m1 = nn.LayerNorm(self.hid_dim)
+        self.fc2 = nn.Linear(self.hid_dim, self.hid_dim)
+        self.m2 = nn.LayerNorm(self.hid_dim)
+        self.fc3 = nn.Linear(self.hid_dim, self.act_dim)
 
-        T.nn.init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='leaky_relu')
+        if self.policy_residual_connection:
+            self.fc_res = nn.Linear(self.obs_dim, self.act_dim)
+
+        self.log_std = T.zeros(1, self.act_dim)
+
+        T.nn.init.zeros_(self.fc1.bias)
+        T.nn.init.zeros_(self.fc2.bias)
+        T.nn.init.zeros_(self.fc3.bias)
+        T.nn.init.kaiming_normal(self.fc1.weight, mode='fan_in', nonlinearity='leaky_relu')
         T.nn.init.kaiming_normal_(self.fc2.weight, mode='fan_in', nonlinearity='leaky_relu')
         T.nn.init.kaiming_normal_(self.fc3.weight, mode='fan_in', nonlinearity='linear')
 
-        if std_fixed:
-            self.log_std = T.zeros(1, self.act_dim)
-        else:
-            self.log_std = nn.Parameter(T.zeros(1, self.act_dim))
+        for p in self.parameters():
+            p.register_hook(lambda grad: T.clamp(grad, -self.grad_clip_value, self.grad_clip_value))
 
     def forward(self, x):
         x = self.activation_fun(self.m1(self.fc1(x)))
         x = self.activation_fun(self.m2(self.fc2(x)))
-        if self.tanh:
-            x = T.tanh(self.fc3(x))
+        if self.policy_residual_connection:
+            x = self.fc3(x) + self.fc_res(x)
         else:
             x = self.fc3(x)
+        if self.tanh:
+            x = T.tanh(x)
         return x
-
-    def soft_clip_grads(self, bnd=1):
-        # Find maximum
-        maxval = 0
-
-        for p in self.parameters():
-            m = T.abs(p.grad).max()
-            if m > maxval:
-                maxval = m
-
-        if maxval > bnd:
-            # print("Soft clipping grads")
-            for p in self.parameters():
-                if p.grad is None: continue
-                p.grad = (p.grad / maxval) * bnd
 
     def sample_action(self, s):
         return T.normal(self.forward(s), T.exp(self.log_std))
-
 
     def log_probs(self, batch_states, batch_actions):
         # Get action means from policy
@@ -75,51 +63,44 @@ class NN_PG(nn.Module):
 
         return log_density.sum(1, keepdim=True)
 
-
 class RNN_PG(nn.Module):
     def __init__(self, env, config):
         super(RNN_PG, self).__init__()
         self.obs_dim = env.obs_dim
         self.act_dim = env.act_dim
-        self.hid_dim = hid_dim
-        self.memory_dim = memory_dim
-        self.tanh = tanh
-        self.to_gpu = to_gpu
 
-        self.rnn = nn.LSTM(self.obs_dim, self.memory_dim, n_temp, batch_first=True)
-        self.fc1 = nn.Linear(self.obs_dim, self.obs_dim)
-        self.m1 = nn.LayerNorm(self.obs_dim)
-        self.fc2 = nn.Linear(self.obs_dim + self.memory_dim, self.hid_dim)
-        self.m2 = nn.LayerNorm(self.hid_dim)
-        self.fc3 = nn.Linear(self.hid_dim, self.act_dim)
+        self.tanh = config["policy_lastlayer_tanh"]
+        self.hid_dim = config["policy_hid_dim"]
+        self.policy_grad_clip_value = config["policy_grad_clip_value"]
+        self.n_lstm_temporal_layers = config["policy_n_lstm_temporal_layers"]
+        self.activation_fun = F.leaky_relu
 
-        if to_gpu:
-            self.log_std_gpu = T.zeros(1, self.act_dim).cuda()
-        else:
-            self.log_std_cpu = T.zeros(1, self.act_dim)
+        self.fc_x_h_1 = nn.Linear(self.obs_dim, self.memory_dim)
+        self.fc_x_h_2 = nn.Linear(self.obs_dim, self.memory_dim)
+        self.fc_x_h_3 = nn.Linear(self.obs_dim, self.memory_dim)
 
+        self.rnn_1 = nn.LSTM(self.memory_dim, self.memory_dim, batch_first=True)
+        self.m_rnn_1 = nn.LayerNorm(self.memory_dim)
+        self.rnn_2 = nn.LSTM(self.memory_dim, self.memory_dim, batch_first=True)
+        self.m_rnn_2 = nn.LayerNorm(self.memory_dim)
+        self.rnn_3 = nn.LSTM(self.memory_dim, self.memory_dim, batch_first=True)
 
-    def print_info(self):
-        pass
+        self.fc_h_y_1 = nn.Linear(self.memory_dim, self.memory_dim)
+        self.fc_h_y_2 = nn.Linear(self.memory_dim, self.memory_dim)
+        self.fc_h_y_3 = nn.Linear(self.memory_dim, self.memory_dim)
+        self.fc_res = nn.Linear(self.obs_dim, self.memory_dim)
 
+        self.log_std = T.zeros(1, self.act_dim)
 
-    def soft_clip_grads(self, bnd=0.5):
-        # Find maximum
-        maxval = 0
+        T.nn.init.zeros_(self.fc1.bias)
+        T.nn.init.zeros_(self.fc2.bias)
+        T.nn.init.zeros_(self.fc3.bias)
+        T.nn.init.kaiming_normal(self.fc1.weight, mode='fan_in', nonlinearity='leaky_relu')
+        T.nn.init.kaiming_normal_(self.fc2.weight, mode='fan_in', nonlinearity='leaky_relu')
+        T.nn.init.kaiming_normal_(self.fc3.weight, mode='fan_in', nonlinearity='linear')
 
         for p in self.parameters():
-            if p.grad is None: continue
-            m = T.abs(p.grad).max()
-            if m > maxval:
-                maxval = m
-
-        if maxval > bnd:
-            #print("Soft clipping grads")
-
-            for p in self.parameters():
-                if p.grad is None: continue
-                p.grad = (p.grad / maxval) * bnd
-
+            p.register_hook(lambda grad: T.clamp(grad, -self.grad_clip_value, self.grad_clip_value))
 
     def forward(self, input):
         x, h = input
@@ -134,11 +115,9 @@ class RNN_PG(nn.Module):
             f = self.fc3(f)
         return f, h
 
-
     def sample_action(self, s):
         x, h = self.forward(s)
         return T.normal(x[0], T.exp(self.log_std_cpu)), h
-
 
     def log_probs(self, batch_states, batch_actions):
         # Get action means from policy
