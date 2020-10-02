e68b7d0ba6bb3ad206bda3f37f8d44476d054fee
silverjoda <heizen.sh@gmail.com>
2020-10-02 14:27:06 +0200

    PG, ES, SB working

--- src/clear_all_data.py
+++ src/clear_all_data.py
@@ -1,24 +1,19 @@
-import os.path
 import os
-import pathlib
+import shutil
+
 AGENTS = True
 TB = True
 
-# everything = pathlib.Path().glob("algos/**")
-# print(list(everything))
-# exit()
-
 filelist = [f for f in os.walk(os.path.join(os.path.dirname(os.path.realpath(__file__)), "algos/"))]
 for root, directories, filenames in filelist:
-    #os.remove(f)
     if root.endswith("agents") or root.endswith("agents_cp") or root.endswith("tb"):
         dirs = list(os.path.join(root, dir) for dir in directories if len(directories) > 0)
         files = list(os.path.join(root, files) for files in filenames if len(filenames) > 0)
 
         for f in files:
-            print(f)
+            os.remove(f)
         for d in dirs:
-            print(d)
+            shutil.rmtree(d)
 
 
 
