4721ad25cba01e518aea32fc8912ec492b555173
silverjoda <heizen.sh@gmail.com>
2020-10-16 20:27:09 +0200

    c

--- src/training/draft.py
+++ src/training/draft.py
@@ -1,51 +1,70 @@
-import torch as T
-import torch.nn as nn
-import copy
-from copy import deepcopy
-import numpy as np
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(4, 8)
-        self.fc2 = nn.Linear(8, 8)
-
-    def forward(self, x):
-        l1 = self.fc1(x)
-        l2 = self.fc2(l1)
-        return l2
-
-class CNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Conv2d(4, 16, 3)
-
-    def forward(self, x):
-        l1 = self.fc1(x)
-        return l1
-
-n1 = Net()
-n2 = Net()
-
-cn1 = CNet()
-cX = T.rand((1, 4, 100, 100))
-cY = cn1(cX)
-
-n1.fc1.weight = nn.Parameter(n2.fc1.weight.clone())
-
-for p1, p2 in zip(n1.parameters(), n2.parameters()):
-    print(id(p1),id(p2))
-
-X = T.rand(4, requires_grad=True)
-X2 = T.tensor(X)
-Y = n1(X)
-Y2 = n2(X2)
-
-Y2.mean().backward()
-Y.mean().backward()
-
-pass
-
-# TODO: Play with second order derivatives and make sure that they work as expected
+import math
 
 
+
+def solution(pegs):
+
+    # Get lower and upper bounds on all gears
+    gear_lbs, gear_ubs = get_bounds(pegs)
+
+    nom = 0
+    denom = 1
+    sol = 0
+
+    while True:
+        if sol > 2:
+            nom += 1
+        else:
+            denom += 1
+
+        if upper_bound_reached():
+            return [-1, -1]
+
+        sol = forward(nom, denom, pegs)
+        if sol == 2:
+            return simplify(nom, denom)
+
+
+def get_bounds(pegs):
+    N = len(pegs)
+
+    # Check just in case they try pull a sneaky
+    for i in range(N - 1):
+        if pegs[i + 1] - pegs[i] < 2:
+            return [-1, -1]
+
+    # List of gear lb and ub
+    gear_lbs = [1] * N
+    gear_ubs = [1] * N
+
+    # Search gears iteratively backwards to calculate ub and lb
+    for i in reversed(range(N)):
+        # Edge case for last peg (rightmost)
+        if i == (N - 1):
+            gear_lbs[i] = 1
+            gear_ubs[i] = pegs[i] - pegs[i - 1] - 1
+
+        dist_to_right_peg = pegs[i + 1] - pegs[i]
+        gear_lbs[i] = dist_to_right_peg - gear_ubs[i + 1]
+        gear_ubs[i] = dist_to_right_peg - gear_lbs[i + 1]
+
+    return gear_lbs, gear_ubs
+
+def upper_bound_reached():
+    pass
+
+def forward(nom, denom, pegs):
+    pass
+
+def simplify(nom, denom):
+    return 0
+
+def intify(nom, denom):
+    return 0
+
+def main():
+    print(solution([4, 30, 50]))
+    print(solution([4, 17, 50]))
+
+if __name__=="__main__":
+    main()
