02e67b9f85e6066aba7095e8f75ce962f28d9fd7
silverjoda <heizen.sh@gmail.com>
2020-10-22 19:58:17 +0200

    c

--- src/envs/bullet_hexapod/limit_setter.py
+++ src/envs/bullet_hexapod/limit_setter.py
@@ -1,10 +1,10 @@
 import fnmatch
-NO_LIMITS = True
-joint_limits = {"coxa" : [-0.4, 0.4],"femur" : [-1.6, -0.6],"tibia" : [0.9, 1.9]}
+NO_LIMITS = False
+joint_limits = {"coxa" : [-0.4, 0.4],"femur" : [-0.5, 0.5],"tibia" : [0.7, 1.7]}
 if NO_LIMITS:
     joint_limits = {"coxa": [-5., 5.], "femur": [-5., 5.], "tibia": [-5., 5.]}
 input_filename = "hexapod.urdf"
-output_filename = "hexapod_wide_range.urdf"
+output_filename = "hexapod_normal.urdf"
 
 with open(input_filename, "r") as in_file: #
     buf = in_file.readlines()
