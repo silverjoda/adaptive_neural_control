0e54ec138e1fd817636f9edc48f2476c74068c9b
silverjoda <heizen.sh@gmail.com>
2021-01-25 08:50:24 +0100

    c

--- src/algos/SB/optuna_baselines_a2c.py
+++ src/algos/SB/optuna_baselines_a2c.py
@@ -11,6 +11,11 @@ def objective(trial):
     config["phase_decay"] = trial.suggest_loguniform('phase_decay', 0.9, 0.99)
     config["z_aux_scalar"] = trial.suggest_uniform('z_aux_scalar', 0.05, 0.1)
 
+    # config["n_steps"] = trial.suggest_int('n_steps', 6, 40)
+    # config["learning_rate"] = "lambda x : x * {}".format(trial.suggest_uniform('learning_rate', 1e-4, 7e-4))
+    # config["gamma"] = trial.suggest_loguniform('gamma', 0.985, 0.999)
+    # config["policy_hid_dim"] = trial.suggest_int("policy_hid_dim", 48, 256)
+
     env, model, _, stats_path = setup_train(config, setup_dirs=True)
     model.learn(total_timesteps=config["iters"])
     env.save(stats_path)
@@ -28,16 +33,16 @@ def objective(trial):
     return avg_episode_rew[0] / config["N_test"]
 
 if __name__ == "__main__":
-    #env_fun = my_utils.import_env("quadrotor_stab")
-    #algo_config = my_utils.read_config("configs/a2c_quadrotor_config.yaml")
-    #env_config = my_utils.read_config("../../envs/bullet_quadrotor/configs/default.yaml")
+    # env_fun = my_utils.import_env("quadrotor_stab")
+    # algo_config = my_utils.read_config("configs/a2c_quadrotor_config.yaml")
+    # env_config = my_utils.read_config("../../envs/bullet_quadrotor/configs/default.yaml")
 
     env_fun = my_utils.import_env("hexapod_wp_eef")
     algo_config = my_utils.read_config("configs/a2c_hexapod_config.yaml")
     env_config = my_utils.read_config("../../envs/bullet_hexapod/configs/eef.yaml")
 
     config = {**algo_config, **env_config}
-    config["iters"] = 10000000
+    config["iters"] = 16000000
     config["verbose"] = False
     config["animate"] = False
     config["default_session_ID"] = "OPT_HEX"
