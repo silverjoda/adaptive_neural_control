29d8053970017d53fad3e096168bdec9ab13c86a
Teymur Azayev <azayetey@fel.cvut.cz>
2021-01-27 17:10:27 +0100

    c

--- src/algos/SB/optuna_baselines_a2c_parallel.py
+++ src/algos/SB/optuna_baselines_a2c_parallel.py
@@ -1,22 +1,10 @@
 import optuna
 from baselines3_RL import *
 from copy import deepcopy
+from multiprocessing import Pool
 
 
-def objective(trial, config):
-    config["n_steps"] = trial.suggest_int('n_steps', 20, 50)
-    config["learning_rate"] = "lambda x : x * {}".format(trial.suggest_uniform('learning_rate', 1e-4, 8e-4))
-    config["gamma"] = trial.suggest_loguniform('gamma', 0.985, 0.999)
-    config["policy_hid_dim"] = trial.suggest_int("policy_hid_dim", 64, 256)
-    config["phase_scalar"] = trial.suggest_uniform('phase_scalar', 0.03, 0.5)
-    config["phase_decay"] = trial.suggest_loguniform('phase_decay', 0.9, 0.99)
-    config["z_aux_scalar"] = trial.suggest_uniform('z_aux_scalar', 0.05, 0.1)
-
-    # config["n_steps"] = trial.suggest_int('n_steps', 6, 40)
-    # config["learning_rate"] = "lambda x : x * {}".format(trial.suggest_uniform('learning_rate', 1e-4, 7e-4))
-    # config["gamma"] = trial.suggest_loguniform('gamma', 0.985, 0.999)
-    # config["policy_hid_dim"] = trial.suggest_int("policy_hid_dim", 48, 256)
-
+def train_and_eval(config):
     env, model, _, stats_path = setup_train(config, setup_dirs=True)
     model.learn(total_timesteps=config["iters"])
     env.save(stats_path)
@@ -33,6 +21,29 @@ def objective(trial, config):
 
     return avg_episode_rew[0] / config["N_test"]
 
+def objective(trial, config):
+    # Hexapod
+    config["n_steps"] = trial.suggest_int('n_steps', 20, 50)
+    config["learning_rate"] = "lambda x : x * {}".format(trial.suggest_uniform('learning_rate', 1e-4, 8e-4))
+    config["gamma"] = trial.suggest_loguniform('gamma', 0.985, 0.999)
+    config["policy_hid_dim"] = trial.suggest_int("policy_hid_dim", 64, 256)
+    config["phase_scalar"] = trial.suggest_uniform('phase_scalar', 0.03, 0.5)
+    config["phase_decay"] = trial.suggest_loguniform('phase_decay', 0.9, 0.99)
+    config["z_aux_scalar"] = trial.suggest_uniform('z_aux_scalar', 0.05, 0.1)
+
+    # Quad
+    # config["n_steps"] = trial.suggest_int('n_steps', 6, 40)
+    # config["learning_rate"] = "lambda x : x * {}".format(trial.suggest_uniform('learning_rate', 1e-4, 7e-4))
+    # config["gamma"] = trial.suggest_loguniform('gamma', 0.985, 0.999)
+    # config["policy_hid_dim"] = trial.suggest_int("policy_hid_dim", 48, 256)
+
+    with Pool(config["N_reps"]) as p:
+        avg_rews = p.map(train_and_eval, [config] * config["N_reps"])
+    print(avg_rews)
+    exit()
+
+    return np.mean(avg_rews)
+
 if __name__ == "__main__":
     # env_fun = my_utils.import_env("quadrotor_stab")
     # algo_config = my_utils.read_config("configs/a2c_quadrotor_config.yaml")
@@ -43,13 +54,16 @@ if __name__ == "__main__":
     env_config = my_utils.read_config("../../envs/bullet_hexapod/configs/eef.yaml")
 
     config = {**algo_config, **env_config}
-    config["iters"] = 12000000
+    config["iters"] = 12000
     config["verbose"] = False
     config["animate"] = False
     config["default_session_ID"] = "OPT_HEX"
     config["tensorboard_log"] = False
     config["N_test"] = 50
+    config["N_reps"] = 5
     N_trials = 50
+
+    # TODO: Demonic processes cannot have children :<
 
     study = optuna.create_study(direction='maximize', study_name="example-study", storage='sqlite:///example.db', load_if_exists=True)
     study.optimize(lambda x : objective(x, config), n_trials=N_trials, show_progress_bar=True)
--- src/envs/bullet_hexapod/draft.py
+++ src/envs/bullet_hexapod/draft.py
@@ -1,22 +1,12 @@
-import torch as T
-import numpy as np
-import matplotlib.pyplot as plt
-from functools import reduce
+import optuna
+import time
 
-figure, axes = plt.subplots()
-N = 30
-fib = lambda n:reduce(lambda x,n:[x[1],x[0]+x[1]], range(n),[0,1])[0]
-#t = [fib(i+2) / 20 for i in range(N)]
-#t = np.linspace(0, 10, N)
-t = [i**np.pi / 100 for i in range(N)]
-print(t)
+def objective(trial):
+    x = trial.suggest_uniform("x", -10, 10)
+    time.sleep(0.01)
+    return (x - 2) ** 2
 
-for i in t:
-    draw_circle = plt.Circle((0.5, i), i, fill=False)
-    plt.gcf().gca().add_artist(draw_circle)
 
-plt.title('Circle')
-axes.set_aspect(1)
-plt.xlim(-10,10)
-plt.ylim(0,20)
-plt.show()
+if __name__ == "__main__":
+    study = optuna.create_study(direction='maximize', study_name="example-study", storage='sqlite:///example.db', load_if_exists=True)
+    study.optimize(objective, n_trials=10000)
