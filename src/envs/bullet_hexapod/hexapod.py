533d7c0d35742fa342bbb08db6a0c27c3c1bda54
silverjoda <heizen.sh@gmail.com>
2021-02-28 20:37:03 +0100

    c

--- src/envs/bullet_hexapod/hexapod.py
+++ src/envs/bullet_hexapod/hexapod.py
@@ -295,7 +295,12 @@ class HexapodBulletEnv(gym.Env):
         # Check if the agent has reached a target
         target_dist = np.sqrt((torso_pos[0] - self.target[0]) ** 2 + (torso_pos[1] - self.target[1]) ** 2)
 
-        r = (self.prev_target_dist - target_dist) * 50 - abs(zd) * 0.2 - abs(phid) * 0.02 - abs(thd) * 0.02
+        if self.config['training_mode'] == "straight":
+            r = (self.prev_target_dist - target_dist) * 50 - abs(zd) * 0.2 - abs(phid) * 0.02 - abs(thd) * 0.02
+        if self.config['training_mode'] == "ccw":
+            r = psid * 1 - xd * 0.1
+        if self.config['training_mode'] == "cw":
+            r = - psid * 1 - xd * 0.1
 
         if target_dist < self.config["target_proximity_threshold"] or (np.abs(torso_pos[0]) > self.target[0]):
             reached_target = True
